58b296e12595970ba33f51528c6930b1458f546c
David <davfash1@gmail.com>
2025-09-08 16:02:47 -0400

    fix(build): add '@' alias and move tailwind/postcss to dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(process.cwd()),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
