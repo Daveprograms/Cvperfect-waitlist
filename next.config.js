5d13f38f09d6406ef0e78ff2ee3b06ad451b3963
David <davfash1@gmail.com>
2025-09-09 08:00:54 -0400

    refactor: simplify Next.js configuration by removing unnecessary options

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  poweredByHeader: false,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(process.cwd()),
-    }
-    return config
-  },
+
 }
 
 module.exports = nextConfig
